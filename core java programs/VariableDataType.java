f28fa1a269a515f18fcd652e425ab43921e93780
indiafreeinternship <indiafreeinternship@gmail.com>
2026-05-23 14:16:19 +0530

    dataType primitive Type source code session 11

---
+++ core java programs/VariableDataType.java
@@ -0,0 +1,20 @@
+class VariableDataType
+{
+    
+    public static void main(String[] args){
+  
+      byte b=127;
+      short s=1234;
+      int i=1234567;
+      long no=9898351245L;
+      float f=123.5F;
+      double d=23344.56;
+
+     System.out.println("b value = "+b);
+     System.out.println("s value = "+s);
+     System.out.println("i value = "+i);
+     System.out.println("no value = "+no);
+     System.out.println("f value ="+f);
+     System.out.println("d value = "+d);
+  }
+}
