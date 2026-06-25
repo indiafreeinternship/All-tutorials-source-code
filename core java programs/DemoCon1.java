851b17ca93d6b27e9cf6a96ad53ddc4a77b01c86
indiafreeinternship <indiafreeinternship@gmail.com>
2026-06-25 23:02:53 +0530

    26. Constructor in Java Full Tutorial | Object Creation Process Explained Step by Step

---
+++ core java programs/DemoCon1.java
@@ -0,0 +1,26 @@
+class CTest1
+{
+	int a=10;
+	
+	
+  	 CTest1(){
+    System.out.println("**********Constructor CTest1() ***********");
+	System.out.println("The value a: "+a);
+   }
+   
+   void dis(){
+	System.out.println("************isntance -dis()*********");
+	System.out.println("The value a:"+ a);
+   }
+	
+}
+
+class DemoCon1 
+{
+	public static void main(String[] args) 
+	{
+		CTest1 ob=new CTest1(); // Con_Call
+		ob.dis();
+		
+	}
+}
