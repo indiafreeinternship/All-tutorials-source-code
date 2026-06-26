fc8ae72946dc5e924d21e72a98045d98f60264d7
indiafreeinternship <indiafreeinternship@gmail.com>
2026-06-26 19:22:00 +0530

    project structure introduction 3 files

---
+++ spring boot source code/BootFirstApp-s20-1/src/main/java/com/app/BootFirstAppS201Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFirstAppS201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootFirstAppS201Application.class, args);
+	}
+
+}
---
+++ spring boot source code/BootFirstApp-s20/src/main/java/com/app/BootFirstAppS20Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFirstAppS20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootFirstAppS20Application.class, args);
+	}
+
+}
