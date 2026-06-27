a293cab0fd9dc5c81a1cf19a1d0702604cbb77a1
indiafreeinternship <indiafreeinternship@gmail.com>
2026-06-27 10:17:44 +0530

    Spring Boot Runners Explained | CommandLineRunner Interface | @Order Annotation | Spring Boot Tutorial session 21

---
+++ spring boot source code/BootRunnerEx-s21/src/main/java/com/app/BootRunnerExS21Application.java
@@ -0,0 +1,15 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRunnerExS21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRunnerExS21Application.class, args);
+		
+		
+	}
+
+}
