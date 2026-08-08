62e95ddc5a27a0734a5f3deafd21a81fa5613d26
indiafreeinternship <indiafreeinternship@gmail.com>
2026-08-08 12:34:28 +0530

    session 35 spring boot

---
+++ spring boot source code/03 Spring Data JPA/05SpringBootDataJPAPagination-S-35/src/main/java/com/app/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="emptab")
+public class Employee {
+	
+	@Id
+	@Column(name="eid")
+	private Integer empId;
+	@Column(name="ename")
+	private String empName;
+	@Column(name="esal")
+	private double empSal;
+
+}
